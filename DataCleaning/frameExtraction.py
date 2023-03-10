af8bc2ed7131697d6828edeadef59420b32efdf5
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-10 14:14:04 +0100

    Commit

--- DataCleaning/frameExtraction.py
+++ DataCleaning/frameExtraction.py
@@ -7,7 +7,7 @@
 # Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
 # Github: https://www.github.com/andreamiele
 # -----
-# Last Modified: Friday, 10th March 2023 2:04:22 pm
+# Last Modified: Friday, 10th March 2023 2:13:44 pm
 # Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
 # -----
 # 
@@ -29,13 +29,13 @@ ROOT_PATH = dirname(dirname(abspath(__file__)))
 if ROOT_PATH not in sys.path:
     sys.path.append(ROOT_PATH)
 
-def listdir_fullpath(d):
+def listing(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 class FrameFolders:
     def __init__(self):
-        self.train_games = listdir_fullpath(ROOT_PATH + "/Data/Train/")
-        self.test_games = listdir_fullpath(ROOT_PATH + "/Data/Test/")
+        self.train_games = listing(ROOT_PATH + "/Data/Train/")
+        self.test_games = listing(ROOT_PATH + "/Data/Test/")
         self.game_folders = self.train_games + self.test_games
     
     def folderscreate(self):  # Top Level
@@ -47,37 +47,37 @@ class FrameFolders:
             if not os.path.exists(frame_folder):
                 os.mkdir(frame_folder)
                 
-    def load_vid_paths(self):  # Top Level
+    def load(self):  # Top Level
         """
         making a list of all Train/Test gameplay.mp4 paths
         """
-        vid_paths = []
+        v_paths = []
         for game_folder in self.game_folders:
-            vid_path = game_folder + "/gameplay.mp4"
-            assert os.path.exists(vid_path)
-            vid_paths.append(vid_path)
-        return vid_paths
+            v_path = game_folder + "/gameplay.mp4"
+            assert os.path.exists(v_path)
+            v_paths.append(v_path)
+        return v_paths
 
-    def save_frames(self, vid_path, n):  # Top Level
+    def save(self, v_path, n):  # Top Level
         """
         running through the video and saving n frames to the frames folder
         """
-        cap = cv2.VideoCapture(vid_path)
+        cap = cv2.VideoCapture(v_path)
         num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        stream = CamGear(source=vid_path).start()
+        stream = CamGear(source=v_path).start()
         save_indices = random.sample(range(0, num_frames), n)
         for i in tqdm(range(num_frames)):
             frame = stream.read()
             if i in save_indices:
-                save_path = vid_path.replace("gameplay.mp4", f"frames/{i}.png")
+                save_path = v_path.replace("gameplay.mp4", f"frames/{i}.png")
                 assert cv2.imwrite(save_path, frame)
                 
     def run(self, n=3000):  # Run
         self.folderscreate()
-        vid_paths = self.load_vid_paths()
-        for i, vid_path in enumerate(vid_paths):
-            print(i, vid_path)
-            self.save_frames(vid_path, n)
+        v_paths = self.load()
+        for i, v_path in enumerate(v_paths):
+            print(i, v_path)
+            self.save(v_path, n)
 
 
 
