d969a4e6c0edfd7e4245e21a0f47923a248a4b0c
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-10 13:38:19 +0100

    Start the frame extraction

--- DataCleaning/frameExtraction.py
+++ DataCleaning/frameExtraction.py
@@ -7,7 +7,7 @@
 # Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
 # Github: https://www.github.com/andreamiele
 # -----
-# Last Modified: Friday, 10th March 2023 1:18:42 pm
+# Last Modified: Friday, 10th March 2023 1:37:58 pm
 # Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
 # -----
 # 
@@ -15,3 +15,19 @@
 # Copyright (c) 2023
 # 
 #  ==============================================================================
+
+
+import os
+import random
+import sys
+from os.path import abspath, dirname
+import cv2
+from tqdm import tqdm
+from vidgear.gears import CamGear
+
+ROOT_PATH = dirname(dirname(abspath(__file__)))
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
+
+def listdir_fullpath(d):
+    return [os.path.join(d, f) for f in os.listdir(d)]
