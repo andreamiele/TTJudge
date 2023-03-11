72071786c1b870619c671db59905e92248176460
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-11 13:50:23 +0100

    Minor changes and path loader

--- Functions/loaders.py
+++ Functions/loaders.py
@@ -7,7 +7,7 @@
 # Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
 # Github: https://www.github.com/andreamiele
 # -----
-# Last Modified: Saturday, 11th March 2023 1:46:59 pm
+# Last Modified: Saturday, 11th March 2023 1:50:06 pm
 # Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
 # -----
 #
@@ -31,10 +31,20 @@ def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 
-def load_json(json_path):  # Runs
-    """
-    simple json load
-    """
-    with open(json_path, "r") as f:
+def load_json(path):
+    with open(path, "r") as f:
         data = json.load(f)
     return data
+
+
+def load_game_folders(train=True, test=True):
+    x = []
+    if train:
+        train_path = ROOT_PATH + "/Data/Train"
+        train_game_paths = listdir_fullpath(train_path)
+        x += train_game_paths
+    if test:
+        test_path = ROOT_PATH + "/Data/Test"
+        test_game_paths = listdir_fullpath(test_path)
+        x += test_game_paths
+    return x
