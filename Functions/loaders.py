cda8dc323aa405cdd304b628c54d8c12e9f1bc53
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-11 21:12:20 +0100

    Add a video stream Loader using cv2

--- Functions/loaders.py
+++ Functions/loaders.py
@@ -7,7 +7,7 @@
 # Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
 # Github: https://www.github.com/andreamiele
 # -----
-# Last Modified: Saturday, 11th March 2023 9:09:51 pm
+# Last Modified: Saturday, 11th March 2023 9:11:51 pm
 # Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
 # -----
 #
@@ -21,6 +21,7 @@ from os.path import abspath, dirname
 import pickle
 import sys
 import json
+import cv2
 
 ROOT_PATH = dirname(dirname(abspath(__file__)))
 if ROOT_PATH not in sys.path:
@@ -75,3 +76,10 @@ def load_labels(train=True, test=True):  # Run
         x += label_paths
 
     return x
+
+
+def videoStreamLoader(path):
+    capture = cv2.VideoCapture(path)
+    nb_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    stream = CamGear(source=path).start()
+    return nb_frames, stream
