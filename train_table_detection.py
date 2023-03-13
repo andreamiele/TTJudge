7c9ae445af5e5c9ffd99d064f2d01a6b244bb1ba
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-13 15:06:05 +0100

    Write UNET __init__

--- train_table_detection.py
+++ train_table_detection.py
@@ -7,7 +7,7 @@
 # Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
 # Github: https://www.github.com/andreamiele
 # -----
-# Last Modified: Monday, 13th March 2023 10:05:55 am
+# Last Modified: Monday, 13th March 2023 3:05:19 pm
 # Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
 # -----
 #
@@ -135,12 +135,25 @@ class DoubleConvolution(nn.Module):
 
 
 class UNET(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        for feature in features:
+            self.downs.append(DoubleConvolution(in_channels, feature))
+            in_channels = feature
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2)
+            )
+            self.ups.append(DoubleConvolution(feature * 2, feature))
+        self.bottleneck = DoubleConvolution(features[-1], features[-1] * 2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     def forward(self, x):
 
-        return self
+        return self.final_conv(x)
 
 
 class Train:
@@ -174,6 +187,7 @@ def wandbSweep():
 
     sweepId = ""
     wandb.agent(sweepId, trainingWandb, count=100)
+
 
 if __name__ == "__main__":
     x = Train()
