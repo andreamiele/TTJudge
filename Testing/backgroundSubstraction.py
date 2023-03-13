4c96b58d753dc6a20f6d95956f11c18cacba7f12
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-13 13:22:32 +0100

    I don't remember

---
+++ Testing/backgroundSubstraction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#  ==============================================================================
+# File: backgroundSubstraction.py
+# Created: Monday, 13th March 2023 10:29:07 am
+# Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
+# Github: https://www.github.com/andreamiele
+# -----
+# Last Modified: Monday, 13th March 2023 10:29:08 am
+# Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
+# -----
+# 
+# -----
+# Copyright (c) 2023
+# 
+#  ==============================================================================
