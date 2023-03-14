5725b84f8a5e0913227a8ee778ed8b338421b1b1
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-14 10:29:03 +0100

    start writing the training

--- train_table_detection.py
+++ train_table_detection.py
@@ -7,7 +7,7 @@
 # Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
 # Github: https://www.github.com/andreamiele
 # -----
-# Last Modified: Monday, 13th March 2023 3:06:16 pm
+# Last Modified: Tuesday, 14th March 2023 10:28:47 am
 # Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
 # -----
 #
@@ -170,8 +170,24 @@ class UNET(nn.Module):
 
 
 class Train:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # * down part
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # * up part
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2))
+            self.ups.append(DoubleConv(feature * 2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1] * 2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     def saveInput(self):
         return self
