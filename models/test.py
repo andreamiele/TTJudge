29f560705b86c4b09f716b374ca794761164b4cf
Andrea Miele <andrea.miele.pro@gmail.com>
2023-03-16 09:46:44 +0100

    Add models folder and will try to do some tests today

---
+++ models/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#  ==============================================================================
+# File: test.py
+# Created: Thursday, 16th March 2023 9:46:11 am
+# Author: Andrea Miele (andrea.miele.pro@gmail.com, https://www.andreamiele.fr)
+# Github: https://www.github.com/andreamiele
+# -----
+# Last Modified: Thursday, 16th March 2023 9:46:12 am
+# Modified By: Andrea Miele (andrea.miele.pro@gmail.com)
+# -----
+# 
+# -----
+# Copyright (c) 2023
+# 
+#  ==============================================================================
